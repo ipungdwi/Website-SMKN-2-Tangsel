1aa05f046be737edea803dc68f015674333d8136
ipungdwiari <c241dsx3663@bangkit.academy>
2024-03-04 23:34:15 +0700

    upd

--- public/index.php
+++ public/index.php
@@ -1,9 +1,9 @@
 <?php
 
-use Illuminate\Contracts\Http\Kernel;
-use Illuminate\Http\Request;
+// use Illuminate\Contracts\Http\Kernel;
+// use Illuminate\Http\Request;
 
-define('LARAVEL_START', microtime(true));
+// define('LARAVEL_START', microtime(true));
 
 /*
 |--------------------------------------------------------------------------
@@ -16,9 +16,9 @@ define('LARAVEL_START', microtime(true));
 |
 */
 
-if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
-    require $maintenance;
-}
+// if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+//     require $maintenance;
+// }
 
 /*
 |--------------------------------------------------------------------------
@@ -31,7 +31,7 @@ if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php'))
 |
 */
 
-require __DIR__.'/../vendor/autoload.php';
+// require __DIR__.'/../vendor/autoload.php';
 
 /*
 |--------------------------------------------------------------------------
@@ -44,12 +44,47 @@ require __DIR__.'/../vendor/autoload.php';
 |
 */
 
-$app = require_once __DIR__.'/../bootstrap/app.php';
+// $app = require_once __DIR__.'/../bootstrap/app.php';
 
-$kernel = $app->make(Kernel::class);
+// $kernel = $app->make(Kernel::class);
 
+// $response = $kernel->handle(
+//     $request = Request::capture()
+// )->send();
+
+// $kernel->terminate($request, $response);
+
+
+define('LARAVEL_START', microtime(true));
+/*
+|--------------------------------------------------------------------------
+| Register The Auto Loader
+|--------------------------------------------------------------------------
+|
+| Composer provides a convenient, automatically generated class loader for
+| our application. We just need to utilize it! We'll simply require it
+| into the script here so that we don't have to worry about manual
+| loading any of our classes later on. It feels great to relax.
+|
+*/
+require __DIR__.'/../laravel/vendor/autoload.php';
+/*
+|--------------------------------------------------------------------------
+| Turn On The Lights
+|--------------------------------------------------------------------------
+|
+| We need to illuminate PHP development, so let us turn on the lights.
+| This bootstraps the framework and gets it ready for use, then it
+| will load up this application so that we can run it and send
+| the responses back to the browser and delight our users.
+|
+*/
+$app = require_once __DIR__.'/../laravel/bootstrap/app.php';
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 $response = $kernel->handle(
-    $request = Request::capture()
-)->send();
-
+    $request = Illuminate\Http\Request::capture()
+);
+$response->send();
 $kernel->terminate($request, $response);
+
+
